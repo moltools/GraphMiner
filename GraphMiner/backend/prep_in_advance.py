30be46cfd47d191ea3359aa0726d5bd13fa0f202
GioviDuif <87034865+GioviDuif@users.noreply.github.com>
2023-03-03 15:46:08 +0100

    ENH: Replacing and returning works for acid group, except for dots

--- GraphMiner/backend/prep_in_advance.py
+++ GraphMiner/backend/prep_in_advance.py
@@ -19,15 +19,19 @@ def select_on_size(smile_mol:str):
     if heavy_atoms <= 20: #Change to 40
         return smile_mol
 
-# def combine_basic_substructures(molsmiles:str):
-#     replacements = {}
-#     return molsmiles, replacements
-
-# def return_basic_substructures(molsmile:str, repl_dicts:dict, index_dicts:dict):
-#     return index_dicts
-
 
 def combine_basic_substructures(molsmiles:str):
+    '''
+    Replace acid group by C and store replacements
+
+    input:
+    molsmiles - SMILES format of a molecule (str)
+
+    returns:
+    mts - SMILES format of a molecule, with combined substructures (str)
+    replacements - dictionary containing as key (int) the index of the remaining
+    atom of the substructure and as value (list of int) the indeces of the replaced atoms
+    '''
     moll = Chem.MolFromSmiles(molsmiles)
     replacements = {}
     if 'C(=O)O' in molsmiles:
@@ -36,7 +40,6 @@ def combine_basic_substructures(molsmiles:str):
         for char in molsmiles[:start_index]:
             if char not in string.ascii_uppercase:
                 atom_index -= 1
-        # PROBLEM: does not work when not last atoms.
         replacements[atom_index] = [atom_index+1, atom_index+2]
         patt = Chem.MolFromSmiles('C(=O)O')
         repl = Chem.MolFromSmiles('C')
@@ -46,56 +49,38 @@ def combine_basic_substructures(molsmiles:str):
         mts = molsmiles
     return mts, replacements
 
+
 def return_basic_substructures(repl_dicts:dict, index_dicts:dict):
+    '''
+    Add in substrucures again and
+
+    input:
+    repl_dicts - dictionary containing as key (int) the index of the remaining
+    atom of the substructure and as value (list of int) the indeces of the replaced atoms
+    index_dicts - dictionary containg as key (int) the subgraph length and as value
+    (list of str) with the subgraphs as strings of indeces divided by '-'
+    
+    returns:
+    index_dicts - dictionary containg as key (int) the subgraph length and as value
+    (list of str) with the subgraphs as strings of indeces divided by '-', but with 
+    substructures indeces placed back in again
+    '''
     for subgraphlength in index_dicts:
         for subgraph in index_dicts[subgraphlength]:
             for value in repl_dicts:
+                length_input = len(repl_dicts[value])
+                index = index_dicts[subgraphlength].index(subgraph)
+                sorted_list = [int(idx) for idx in subgraph.split('-')]
+                #Replace all values that are higher than the one for which the 
+                #subgraph will be added in
+                for ind in range(len(sorted_list)):
+                    if sorted_list[ind] > value:
+                        sorted_list[ind] = sorted_list[ind] + length_input
+                #Add in the earlier combined subgraphs
                 if str(value) in subgraph.split('-'):
-                    index = index_dicts[subgraphlength].index(subgraph)
-                    list_val = subgraph.split('-')
-                    sorted_list = [int(indexs) for indexs in list_val]
-                    length_input = len(repl_dicts[value])
-                    # print('before: ' + str(sorted_list))
-                    for ind in range(len(sorted_list)):
-                        if sorted_list[ind] > value:
-                            sorted_list[ind] = sorted_list[ind] + length_input
-                    # print('after add: ' + str(sorted_list))
-                    #ADD IN OTHER INDECES AND REPLACE
                     sorted_list += (repl_dicts[value])
                     sorted_list.sort()
-                    # print('after including: ' + str(sorted_list))
-                    # print(' ')
-                    sorted_list_str = [str(val) for val in sorted_list]
-                    new = '-'.join(sorted_list_str)
-                    index_dicts[subgraphlength][index] = new
+                sorted_list_str = [str(val) for val in sorted_list]
+                new = '-'.join(sorted_list_str)
+                index_dicts[subgraphlength][index] = new
     return index_dicts
-
-
-
-    for subgraphlength in index_dicts:
-        for subgraph in index_dicts[subgraphlength]:
-            for value in repl_dicts:
-                if str(value) in subgraph.split('-'):
-                    index = index_dicts[subgraphlength].index(subgraph)
-                    list_val = subgraph.split('-')
-                    sorted_list = [int(indexs) for indexs in list_val]
-                    for number in repl_dicts[value]:
-                        if number in sorted_list:
-                            for ind in range(len(sorted_list)):
-                                if sorted_list[ind] >= number:
-                                    sorted_list[ind] += 1         
-                    sorted_list += (repl_dicts[value])
-                    sorted_list.sort()
-                    sorted_list_str = [str(val) for val in sorted_list]
-                    new = '-'.join(sorted_list_str)
-                    index_dicts[subgraphlength][index] = new
-                else:
-                    for number in repl_dicts[value]:
-                        sorted_list = [int(index) for index in subgraph.split('-')]
-                        if number in sorted_list:
-                            for ind in range(len(sorted_list)):
-                                if sorted_list[ind] >= number:
-                                    sorted_list[ind] += 1   
-                            sorted_list_str = [str(val) for val in sorted_list]
-                            new = '-'.join(sorted_list_str)
-                            index_dicts[subgraphlength][index] = new    
