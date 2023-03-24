66868e90e0a4bba9be47c7c8335beec50dfb1a91
GioviDuif <87034865+GioviDuif@users.noreply.github.com>
2023-03-24 14:24:12 +0100

    ENH: clean up main file

--- main_script.py
+++ main_script.py
@@ -12,10 +12,10 @@ dict_of_data = create_dict(grouplist, infile)
 
 ### PREPARATION OF SMILES + GRAPH MINING ###
 
-## Working with RWMol
-from GraphMiner import select_on_size, breadth_fs, rdkit_smiles, combine_substr, \
-    repl_atommap_COO, set_atommapnum, rdkit_parse_atommap, return_replaced, repl_atommap_CO, \
-    repl_atommap_C_O, count_freq, list_maker, repl_atommap_NCO
+from GraphMiner import select_on_size, breadth_fs, rdkit_smiles, \
+    combine_substr, repl_atommap_COO, set_atommapnum, rdkit_parse_atommap, \
+    return_replaced, repl_atommap_CO, repl_atommap_C_O, count_freq, \
+    list_maker, repl_atommap_NCO
 
 for group in grouplist:
     list_of_smiles = dict_of_data[group]
@@ -71,56 +71,6 @@ for group in grouplist:
     #     writer.writerow(row)
     # f.close()
 
-# from GraphMiner import combining, returning
-# for group in grouplist:
-#     list_of_smiles = dict_of_data[group]
-#     for mol_smile in list_of_smiles:
-#         selected_smile = select_on_size(mol_smile)
-#         if selected_smile == None:
-#             continue
-#         print(' ')
-#         com_mol_smiles, new_indeces = combining(Chem.MolToSmiles(selected_smile))
-#         print(com_mol_smiles)
-#         dictnode, list_node = rdkit_parse(com_mol_smiles)
-#         # subgraphdict = breadth_fs(list_node, dictnode)
-#         # print(subgraphdict)
-#         repl_smile = returning(com_mol_smiles, new_indeces)
-#         print(repl_smile)
-
-
-## Write second type CSV file
-from GraphMiner import list_maker
-
-# for group in grouplist:
-#     list_of_smiles = dict_of_data[group]
-#     all_substr = []
-#     dict_substr = {}
-#     total_molecules = 0
-#     for mol_smile in list_of_smiles:
-#         # print('tot: ' + str(total_molecules))
-#         selected_mol = select_on_size(mol_smile)
-#         if selected_mol == None:
-#             continue
-#         selected_smile = Chem.MolToSmiles(selected_mol)
-#         dictnode, list_node = rdkit_parse(selected_smile)
-#         subgraphdict = breadth_fs(list_node, dictnode)
-#         smilesdict = rdkit_smiles(subgraphdict, selected_smile)
-#         unique_str = combine_substr(smilesdict)
-#         print(unique_str)
-#         all_substr += (unique_str)
-#         dict_substr[total_molecules] = unique_str
-#         total_molecules += 1
-#     print(group)
-#     counts = count_freq(all_substr)
-#     list_of_rows = list_maker(dict_substr, counts)
-#     name = 'New_overview_group' + str(group) +'.csv'
-#     f = open(name,  'w')
-#     writer = csv.writer(f)
-#     Head_row = ('Substructure', 'Frequency' + str(group), 'OccurrenceList' + str(group))
-#     writer.writerow(Head_row)
-#     for row in list_of_rows:
-#         writer.writerow(row)
-#     f.close()
 
 ### STATISTICS PART ###
 
