daf3b1f3c1a5a948072928ee60b7807b654cd2c5
David Meijer <david-meijer@live.nl>
2023-06-23 11:17:15 +0200

    CHK: small changes to imports and reporting

--- main_script.py
+++ main_script.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
+from logging import Logger, StreamHandler
+
+TIMEOUT_TIME = 1  # change this to change the timeout time in seconds
+LOGGER_LEVEL = 'INFO'  # skips debug messages
+# LOGGER_LEVEL = 'DEBUG'  # prints debug messages to stdout
+SIGNIFICANCE_LEVEL = 0.5
 
 import csv
 import time
-start = time.time()
+start = time.time()  # should be moved to main() function
 from rdkit import Chem
 import pandas as pd
 import numpy as np
+
+from tqdm import tqdm 
 
 
 ### DATA LOADING ###
 from GraphMiner import load_data, determine_groups, create_dict
 
-infile = load_data(1, ';')
+infile = load_data(1, ',')  # would use argparse, or index argv here directly and not send index as parameter
+                            # make sure parsing of any command line parameter happens right at start of script in main function
 grouplist = determine_groups(infile)
 dict_of_data = create_dict(grouplist, infile)
 
@@ -25,50 +34,56 @@ from GraphMiner import select_on_size, \
     rdkit_smiles2, repl_atommap_SOO, repl_atommap_SOOO, timeout, return_replaced3, \
     rdkit_smiles3, combine_substr2, TimeoutError, repl_atommap_POOOO
 
+# move all imports to top of script
+from GraphMiner import hypergeometric_test_pval, \
+    mul_test_corr, extract_signif_substr, create_groups_substr, \
+    mol_to_fingerprint, plot_dendrogram, create_groups_dendrogram, \
+    draw_mol_fig,tanimoto_coefficient
 
-@timeout(30)
+
+@timeout(TIMEOUT_TIME)
 def mol_substr_bfs(selected_mol, all_substr, dict_substr, total_molecules):
-    print(' ')
+    logger.debug(' ')
     repl = {}
     sel_smile = Chem.MolToSmiles(selected_mol, kekuleSmiles = True)
     sel_mol = Chem.MolFromSmiles(sel_smile)
-    print('START: ' + sel_smile)
+    logger.debug('START: ' + sel_smile)
     set_atommapnum(sel_mol)
     tot_mol = sel_mol
     if sel_mol.HasSubstructMatch(Chem.MolFromSmiles('C(=O)O')) == True:
         sel_mol, repl = repl_atommap_COO(sel_mol, repl)
-        print('C(=O)O done')
+        logger.debug('C(=O)O done')
         # print(Chem.MolToSmiles(sel_mol))
     if sel_mol.HasSubstructMatch(Chem.MolFromSmiles('P(=O)(O)(O)O')) == True:
         sel_mol, repl = repl_atommap_POOOO(sel_mol, repl)
-        print('P(=O)(O)(O)O done')
+        logger.debug('P(=O)(O)(O)O done')
         # print(Chem.MolToSmiles(sel_mol))
     if sel_mol.HasSubstructMatch(Chem.MolFromSmiles('P(=O)(O)O')) == True:
         sel_mol, repl = repl_atommap_POOO(sel_mol, repl)
-        print('P(=O)(O)O done')
+        logger.debug('P(=O)(O)O done')
         # print(Chem.MolToSmiles(sel_mol))
     if sel_mol.HasSubstructMatch(Chem.MolFromSmiles('S(=O)(=O)O')) == True:
         sel_mol, repl = repl_atommap_SOOO(sel_mol, repl)
-        print('S(=O)(=O)O done')
+        logger.debug('S(=O)(=O)O done')
         # print(Chem.MolToSmiles(sel_mol))
     if sel_mol.HasSubstructMatch(Chem.MolFromSmiles('S(=O)(=O)')) == True:
         sel_mol, repl = repl_atommap_SOO(sel_mol, repl)
-        print('S(=O)(=O) done')
+        logger.debug('S(=O)(=O) done')
     if sel_mol.HasSubstructMatch(Chem.MolFromSmiles('N(O)C(=O)')) == True:
         sel_mol, repl = repl_atommap_NOCO(sel_mol, repl)
-        print('NOCO done')
+        logger.debug('NOCO done')
         # print(Chem.MolToSmiles(sel_mol))
     if sel_mol.HasSubstructMatch(Chem.MolFromSmiles('NC=O')) == True:
         sel_mol, repl = repl_atommap_NCO(sel_mol, repl)
-        print('NC=O done')
+        logger.debug('NC=O done')
         # print(Chem.MolToSmiles(sel_mol))
     if sel_mol.HasSubstructMatch(Chem.MolFromSmiles('CO')) == True:
         sel_mol, repl = repl_atommap_CO(sel_mol, repl)
-        print('CO done')
+        logger.debug('CO done')
         # print(Chem.MolToSmiles(sel_mol))
     if sel_mol.HasSubstructMatch(Chem.MolFromSmiles('C=O')) == True:
         sel_mol, repl = repl_atommap_C_O(sel_mol, repl)
-        print('C=O done')
+        logger.debug('C=O done')
         # print(Chem.MolToSmiles(sel_mol))
     dictnode, list_node = rdkit_parse_atommap(sel_mol)
     subgraphdict = breadth_fs2(dictnode, list_node)
@@ -80,13 +95,14 @@ def mol_substr_bfs(selected_mol, all_substr, dict_substr, total_molecules):
     total_molecules += 1
     return dict_substr, total_molecules, all_substr
 
-@timeout(30)
+
+@timeout(TIMEOUT_TIME)
 def mol_substr_dfs(selected_mol, all_substr, dict_substr, total_molecules):
     print(' ')
     repl = {}
     sel_smile = Chem.MolToSmiles(selected_mol, kekuleSmiles = True)
     sel_mol = Chem.MolFromSmiles(sel_smile)
-    print('START: ' + sel_smile)
+    logger.debug('START: ' + sel_smile)
     set_atommapnum(sel_mol)
     tot_mol = sel_mol
     # if sel_mol.HasSubstructMatch(Chem.MolFromSmiles('C(=O)O')) == True:
@@ -131,156 +147,170 @@ def mol_substr_dfs(selected_mol, all_substr, dict_substr, total_molecules):
     total_molecules += 1
     return dict_substr, total_molecules, all_substr
 
-number = 0
-group_num = 0
-len_dict = {}
-list_of_groups = {}
-list_of_df = []
-TO = 0
-TOlist = []
-tottlist = []
-for group in grouplist:
-    list_of_smiles = dict_of_data[group]
-    all_substr = []
-    dict_substr = {}
-    group_tot = 0
-    for mol_smile in list_of_smiles:
-        first_select = select_on_size(mol_smile, 40)
-        selected_mol = select_mol(first_select)
-        if selected_mol == None:
-            continue
-        elif type(selected_mol) == list:
-            print('list found')
-            number += 1
-            print(number)
-            for mol in selected_mol:
-                try:
-                    dict_substr, group_tot, all_substr = mol_substr_bfs(Chem.MolFromSmiles(mol), all_substr, dict_substr, group_tot)
-                    tottlist.append(Chem.MolFromSmiles(mol).GetNumHeavyAtoms())
-                except TimeoutError:
-                    print('timeout')
-                    TO += 1
-                    TOlist.append(Chem.MolFromSmiles(mol).GetNumHeavyAtoms())
-                    continue
-        else:
-            number += 1
-            try:
-                dict_substr, group_tot, all_substr = mol_substr_bfs(selected_mol, all_substr, dict_substr, group_tot)
-                tottlist.append(selected_mol.GetNumHeavyAtoms())
-            except TimeoutError:
-                print('timeout')
-                TO += 1
-                TOlist.append(selected_mol.GetNumHeavyAtoms())
+
+def main(logger: Logger) -> None:
+    number = 0
+    group_num = 0
+    len_dict = {}
+    list_of_groups = {}
+    list_of_df = []
+    TO = 0
+    TOlist = []
+    tottlist = []
+    for group in tqdm(grouplist, desc='Group', leave=False):
+        list_of_smiles = dict_of_data[group]
+        all_substr = []
+        dict_substr = {}
+        group_tot = 0
+        for mol_smile in tqdm(list_of_smiles, desc='Molecule', leave=False):
+            first_select = select_on_size(mol_smile, 40)
+            selected_mol = select_mol(first_select)
+            if selected_mol == None:
                 continue
-    list_of_groups[group] = group_tot
-    counts = count_freq(all_substr)
-    count_dict = {}
-    id=0
-    for substr in counts:
-        count_dict[id] = [substr, counts[substr]]
-        id+=1
-    columnnames = ['Substructure', 'Frequency' + str(group_num)]
-    df = pd.DataFrame.from_dict(count_dict, orient='index', columns=columnnames)
-    print('df')
-    print(df)
-    list_of_df.append(df)
-    group_num += 1
-    print(list_of_df)
-print('TO', TO)
-print(TOlist)
-print(tottlist)
-if len(list_of_df) == 1:
-    list_of_df[0].to_csv('substrfile3.csv', header = ['Substructure', 'Frequency'])
-elif len(list_of_df) == 2:
-    joined_df = pd.merge(list_of_df[0], list_of_df[1], how='outer')
-    colmn = list(joined_df.columns)
-    joined_df[colmn[1]] = joined_df[colmn[1]].replace(np.nan, 0)
-    joined_df[colmn[2]] = joined_df[colmn[2]].replace(np.nan, 0)
-    headers = ['Substructure']
-    for groupname in grouplist:
-        headers.append('Frequency' + str(groupname))
-    print(headers)
-    joined_df.to_csv('substrfile3.csv', header = headers)
-elif len(list_of_df) >=3:
-    joined_df = pd.merge(list_of_df[0], list_of_df[1], how='outer')
-    for dfnum in range(2, len(list_of_df)):
-        joined_df = pd.merge(joined_df, list_of_df[dfnum], how='outer')
-    print(joined_df)
-    colmn = list(joined_df.columns)
-    headers = ['Substructure']
-    for num_df in range(len(list_of_df)):
-        joined_df[colmn[num_df + 1]] = joined_df[colmn[num_df + 1]].replace(np.nan, 0)
-    for groupname in grouplist:
-        headers.append('Frequency' + str(groupname))
-    print(headers)
-    joined_df.to_csv('substrfile3.csv',header=headers)
+            elif type(selected_mol) == list:
+                logger.debug('list found')
+                number += 1
+                logger.debug(number)
+                for mol in selected_mol:
+                    try:
+                        dict_substr, group_tot, all_substr = mol_substr_bfs(Chem.MolFromSmiles(mol), all_substr, dict_substr, group_tot)
+                        tottlist.append(Chem.MolFromSmiles(mol).GetNumHeavyAtoms())
+                    except TimeoutError:
+                        logger.debug('timeout')
+                        TO += 1
+                        TOlist.append(Chem.MolFromSmiles(mol).GetNumHeavyAtoms())
+                        continue
+            else:
+                number += 1
+                try:
+                    dict_substr, group_tot, all_substr = mol_substr_bfs(selected_mol, all_substr, dict_substr, group_tot)
+                    tottlist.append(selected_mol.GetNumHeavyAtoms())
+                except TimeoutError:
+                    logger.debug('timeout')
+                    TO += 1
+                    TOlist.append(selected_mol.GetNumHeavyAtoms())
+                    continue
+        list_of_groups[group] = group_tot
+        counts = count_freq(all_substr)
+        count_dict = {}
+        id=0
+        for substr in counts:
+            count_dict[id] = [substr, counts[substr]]
+            id+=1
+        columnnames = ['Substructure', 'Frequency' + str(group_num)]
+        df = pd.DataFrame.from_dict(count_dict, orient='index', columns=columnnames)
+        logger.debug('df')
+        logger.debug(df)
+        list_of_df.append(df)
+        group_num += 1
+        logger.debug(list_of_df)
+    logger.debug('TO', TO)
+    logger.debug(TOlist)
+    logger.debug(tottlist)
+    if len(list_of_df) == 1:
+        list_of_df[0].to_csv('substrfile3.csv', header = ['Substructure', 'Frequency'])
+    elif len(list_of_df) == 2:
+        joined_df = pd.merge(list_of_df[0], list_of_df[1], how='outer')
+        colmn = list(joined_df.columns)
+        joined_df[colmn[1]] = joined_df[colmn[1]].replace(np.nan, 0)
+        joined_df[colmn[2]] = joined_df[colmn[2]].replace(np.nan, 0)
+        headers = ['Substructure']
+        for groupname in grouplist:
+            headers.append('Frequency' + str(groupname))
+        logger.debug(headers)
+        joined_df.to_csv('substrfile3.csv', header = headers)
+    elif len(list_of_df) >=3:
+        joined_df = pd.merge(list_of_df[0], list_of_df[1], how='outer')
+        for dfnum in range(2, len(list_of_df)):
+            joined_df = pd.merge(joined_df, list_of_df[dfnum], how='outer')
+        logger.debug(joined_df)
+        colmn = list(joined_df.columns)
+        headers = ['Substructure']
+        for num_df in range(len(list_of_df)):
+            joined_df[colmn[num_df + 1]] = joined_df[colmn[num_df + 1]].replace(np.nan, 0)
+        for groupname in grouplist:
+            headers.append('Frequency' + str(groupname))
+        logger.debug(headers)
+        joined_df.to_csv('substrfile3.csv',header=headers)
 
-f = open('datafilepart13.csv', 'w')
-writer = csv.writer(f)
-writer.writerow(grouplist)
-writer.writerow(list_of_groups)
-f.close()
+    f = open('datafilepart13.csv', 'w')
+    writer = csv.writer(f)
+    writer.writerow(grouplist)
+    writer.writerow(list_of_groups)
+    f.close()
 
-### STATISTICS PART ###
+    ### STATISTICS PART ###
 
-# Load in csv files
-substr_df = load_data(2, ',')
+    # Load in csv files
+    substr_df = load_data(2, ',')
 
-## Calculate p values
-from GraphMiner import hypergeometric_test_pval, \
-    mul_test_corr, extract_signif_substr, create_groups_substr, \
-    mol_to_fingerprint, plot_dendrogram, create_groups_dendrogram, \
-    draw_mol_fig,tanimoto_coefficient
+    ## Calculate p values
+    # from GraphMiner import hypergeometric_test_pval, \
+    #     mul_test_corr, extract_signif_substr, create_groups_substr, \
+    #     mol_to_fingerprint, plot_dendrogram, create_groups_dendrogram, \
+    #     draw_mol_fig,tanimoto_coefficient
 
-pvaldict = hypergeometric_test_pval(list_of_groups, substr_df, grouplist)
-for key in pvaldict.keys():
-    substr_df[key] = pvaldict[key]
+    pvaldict = hypergeometric_test_pval(list_of_groups, substr_df, grouplist)
+    for key in pvaldict.keys():
+        substr_df[key] = pvaldict[key]
 
-f = open('pvaloverview3.csv',  'w')
-writer = csv.writer(f)
-for row in substr_df.iterrows():
-    writer.writerow(row)
-f.close()
+    f = open('pvaloverview3.csv',  'w')
+    writer = csv.writer(f)
+    for row in substr_df.iterrows():
+        writer.writerow(row)
+    f.close()
 
-f = open('significantsubstr3.csv', 'w')
-writer = csv.writer(f)
-p = 0
-for pvallist in pvaldict.values():
-    writer.writerow(['New Group'])
-    writer.writerow([p])
-    p +=1
-    TF_benj_list = mul_test_corr(pvallist, 'fdr_bh')
-    substr_df['True/False Benj-Hoch'] = TF_benj_list
-    list_sigdif = extract_signif_substr(TF_benj_list, substr_df)
-    writer.writerow(list_sigdif)
-    # dic_of_substr = create_groups_substr(list_sigdif)
-    # writer.writerow(dic_of_substr)
-    # smilessubstr = [smiles for smiles in list_sigdif]
-    # smilessubstr = []
-    # for smiless in list_sigdif:
-    #     smiless.replace('c', 'C')
-    #     smiless.replace('n', 'N')
-    #     smiless.replace('o', 'O')
-    #     smiless.replace('s', 'S')
-    #     smilessubstr.append(smiless)
-    # molsubstr = [Chem.MolFromSmiles(smiles) for smiles in smilessubstr]
-    # fps = [mol_to_fingerprint(mol) for mol in molsubstr]
-    # dist_m = np.zeros((len(list_sigdif), len(list_sigdif)))
-    # for i, fp_i in enumerate(fps):
-    #     for j, fp_j in enumerate(fps):
-    #         if j > i:
-    #             coef = 1 - tanimoto_coefficient(fp_i, fp_j)
-    #             dist_m[i, j] = coef
-    #             dist_m[j, i] = coef
-    # namefile = '/home/duive014/MOLTOOLS/GraphMiner/Images/' + str(groupname) + '_dendrogram.png'
-    # dendrogram = plot_dendrogram(dist_m, smilessubstr, namefile)
-    # valueslist = create_groups_dendrogram(dendrogram)
-    # writer.writerow(valueslist)
-    # filepaths = '/home/duive014/MOLTOOLS/GraphMiner/Images' + '/' + str(groupname)
-    # draw_mol_fig(valueslist, filepaths)
-f.close()
-end = time.time()
-print('time', end-start)
-print('TO', TO)
-print('number', number)
+    f = open('significantsubstr3.csv', 'w')
+    writer = csv.writer(f)
+    p = 0
+    for pvallist in pvaldict.values():
+        writer.writerow(['New Group'])
+        writer.writerow([p])
+        p +=1
+        TF_benj_list = mul_test_corr(pvallist, 'fdr_bh')
+        substr_df['True/False Benj-Hoch'] = TF_benj_list
+        list_sigdif = extract_signif_substr(TF_benj_list, substr_df)
+        writer.writerow(list_sigdif)
+        dic_of_substr = create_groups_substr(list_sigdif)
+        writer.writerow(dic_of_substr)
+        smilessubstr = [smiles for smiles in list_sigdif]
+        smilessubstr = []
+        for smiless in list_sigdif:
+            smiless.replace('c', 'C')
+            smiless.replace('n', 'N')
+            smiless.replace('o', 'O')
+            smiless.replace('s', 'S')
+            smilessubstr.append(smiless)
+        molsubstr = [Chem.MolFromSmiles(smiles) for smiles in smilessubstr]
+        fps = [mol_to_fingerprint(mol) for mol in molsubstr]
+        dist_m = np.zeros((len(list_sigdif), len(list_sigdif)))
+        for i, fp_i in enumerate(fps):
+            for j, fp_j in enumerate(fps):
+                if j > i:
+                    coef = 1 - tanimoto_coefficient(fp_i, fp_j)
+                    dist_m[i, j] = coef
+                    dist_m[j, i] = coef
 
+        print(dist_m)
+        exit("remove later")
+
+        namefile = '/home/duive014/MOLTOOLS/GraphMiner/Images/' + str(groupname) + '_dendrogram.png'
+        dendrogram = plot_dendrogram(dist_m, smilessubstr, namefile)
+        valueslist = create_groups_dendrogram(dendrogram)
+        writer.writerow(valueslist)
+        filepaths = '/home/duive014/MOLTOOLS/GraphMiner/Images' + '/' + str(groupname)
+        draw_mol_fig(valueslist, filepaths)
+    f.close()
+    end = time.time()
+    logger.debug('time', end-start)
+    logger.debug('TO', TO)
+    logger.debug('number', number)
+
+
+if __name__ == "__main__":
+    logger = Logger('main_script.py', LOGGER_LEVEL)
+    stream_handler = StreamHandler()
+    stream_handler.setLevel(LOGGER_LEVEL)
+    logger.addHandler(stream_handler)
+
+    main(logger)
