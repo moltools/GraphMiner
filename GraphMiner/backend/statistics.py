45a71cff7baf06196cbc95b8ee8f3055e1aca278
GioviDuif <87034865+GioviDuif@users.noreply.github.com>
2023-07-06 11:11:15 +0200

    hypergeometrictest

--- GraphMiner/backend/statistics.py
+++ GraphMiner/backend/statistics.py
@@ -20,6 +20,7 @@ def hypergeometric_test_pval(total_dict: dict, input_df, grouplist):
         N = total_dict[val]
         for substr in input_df.iterrows():
             for input in substr:
+                print(input)
                 if type(input) != int:
                     k = input[group+1]
                     s = 0
